55d3bdfc507e9d7c5659fe9f1c801fd4d2460f0b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-18 18:59:23 -0700

    Restructure project info to config folder

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  title: string;
+  summary: string;
+  img: {
+    src: ImageMetadata;
+    alt: string;
+  };
+  links: {
+    website?: string;
+    github: string;
+  };
+}
