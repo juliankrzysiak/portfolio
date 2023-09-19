70b01c42ad786c655aaab1067492210d4a454fca
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-18 17:27:15 -0700

    Add self-hosted fonts
    Set the default font everywhere to Lato

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
