510846f151c757c1903a79ec6d0a19f8c4cbe5ff
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-01-17 23:39:03 -0800

    Update file structure
    Organized based on features. Also added import alias. I also had to
    remove image optimization using Astro since I don't want to hard code
    every project image.

--- src/features/home/data/projects.ts
+++ src/features/home/data/projects.ts
@@ -1,16 +1,17 @@
 import type { Project } from '../types';
 
-import roam from '/src/assets/roam_screenshot.png';
-import hydration from '/src/assets/hydration_screenshot.png';
-import books from '/src/assets/tomeTracker_screenshot.png';
-import jiaolongart from '/src/assets/jiaolongart_screenshot.png';
+import roam from '../assets/roam_screenshot.png';
+import hydration from '../assets/hydration_screenshot.png';
+import books from '../assets/tomeTracker_screenshot.png';
+import jiaolongart from '../assets/jiaolongart_screenshot.png';
 
-export const projects: Project[] = [
+
+const projects: Project[] = [
   {
     title: 'JiaolongArt',
     summary: "Commissioned website for showcasing artist's select portfolio.",
     img: {
-      src: jiaolongart,
+      src: jiaolongart.src,
     },
     links: {
       website: 'https://jiaolongart.com/',
@@ -21,7 +22,7 @@ export const projects: Project[] = [
     summary:
       'A road trip planning website that maps out every destination you want to go to, for every day of your trip.',
     img: {
-      src: roam,
+      src: roam.src,
     },
     links: {
       github: 'https://github.com/CastillejaCode/journey',
@@ -32,7 +33,7 @@ export const projects: Project[] = [
     summary:
       'Record when you watered your plants and when you need to water them next using a calendar.',
     img: {
-      src: hydration,
+      src: hydration.src,
     },
     links: {
       website: 'https://water-schedule.pages.dev',
@@ -44,10 +45,12 @@ export const projects: Project[] = [
     summary:
       "Track what books you've read. Input ratings, reviews, finish dates, filter and search for books, and more.",
     img: {
-      src: books,
+      src: books.src,
     },
     links: {
       github: 'https://github.com/CastillejaCode/book-list',
     },
   },
 ];
+
+export default projects
